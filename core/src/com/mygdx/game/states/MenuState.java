31f66a1628b4487271b8c502db2da346a747446e
Karol011 <karollacek1@gmail.com>
2020-05-22 12:54:35 +0200

    after pressing button, game transist into playmode

--- core/src/com/mygdx/game/states/MenuState.java
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.FlappyBird;
@@ -16,19 +17,28 @@ public class MenuState extends State {
 
     @Override
     public void handleInput() {
-
+        if (Gdx.input.justTouched()) {
+            gsm.set(new PlayState(gsm));
+            dispose();
+        }
     }
 
     @Override
     public void update(final float dt) {
-
+        handleInput();
     }
 
     @Override
     public void render(final SpriteBatch sb) {
         sb.begin();
-        sb.draw(background,0,0, FlappyBird.WIDTH,FlappyBird.HEIGHT);
-        sb.draw(playBtn,(FlappyBird.WIDTH/2) - (playBtn.getWidth()/2),FlappyBird.HEIGHT/2);
+        sb.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
+        sb.draw(playBtn, (FlappyBird.WIDTH / 2) - (playBtn.getWidth() / 2), FlappyBird.HEIGHT / 2);
         sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playBtn.dispose();
     }
 }
